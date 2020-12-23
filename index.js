644f91da071a6ae53e738b697e73d27e2122928b
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2020-12-22 18:57:59 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var server = require("http");
+console.log("Testrr");
+server
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
